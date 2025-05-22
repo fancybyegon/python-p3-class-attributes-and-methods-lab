ad042c1af32d687e4181d758a26efd695d3eeb59
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-05-22 21:06:03 +0300

    class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,33 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+    
+    songs = [
+        ("99 Problems", "Jay Z", "Rap"),
+        ("Halo", "Beyonce", "Pop"),
+        ("Smells Like Teen Spirit", "Nirvana", "Rock")
+    ]
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        Song.count += 1
+        
+        self.genres.add(genre)
+        self.artists.add(artist)
+        
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+            
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+            
+    
